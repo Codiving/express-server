4901f31777442b3c0027105f4110ad51bb914b6c
Codiving <codivingmoon@gmail.com>
2021-08-27 19:07:14 +0900

    get 함수 추가

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ import express from "express";
 const PORT = 3000; // 서버에 사용할 포트 번호
 const app = express();
 
+app.get("/", (req, res) => {
+  console.log("req.body", req.body);
+  res.send(`<h1>HTML H1</h1>`);
+});
+
 app.listen(PORT, () =>
   console.log(`Express server : http://localhost:${PORT}`)
 );
