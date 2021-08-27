c1c462441564361a7cc352f7ec8467124d380261
Codiving <codivingmoon@gmail.com>
2021-08-27 19:39:28 +0900

    Bugfix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = 3000; // 서버에 사용할 포트 번호
 const app = express();
 
 app.get("/", (req, res) => {
-  console.log("req.body", req.body);
+  console.log("req.query", req.query);
   res.send(`<h1>HTML H1</h1>`);
 });
 
