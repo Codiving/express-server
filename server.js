8ea40a11d611e4f7d3d1eae559634bec42e9f5f8
Codiving <codivingmoon@gmail.com>
2021-08-27 20:02:08 +0900

    post 함수

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ import express from "express";
 const PORT = 3000; // 서버에 사용할 포트 번호
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   console.log("req.query", req.query);
+  res.send(`<h1>HTML H1</h1>`);
+});
+
+app.post("/", (req, res) => {
+  console.log("req.body", req.body);
   res.send(`<h1>HTML H1</h1>`);
 });
 
